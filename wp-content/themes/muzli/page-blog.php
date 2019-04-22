d26c8e73869f7d30cc5bd15b3dc2913b479439c2
ondrii <ondrimail@gmail.com>
2019-04-22 14:32:52 +0200

    Lesson#34: Filtrovanie dát z databázy, ako PHPčkom odpáliť shortcode

--- wp-content/themes/muzli/page-blog.php
+++ wp-content/themes/muzli/page-blog.php
@@ -7,11 +7,37 @@
 
         <div class="post-list">
             <?php 
-                // $posts = get_posts();
+
+                // echo '<pre>';
+                // print_r( wp_load_alloptions() );
+                // echo'</pre>';
+
+                $posts = get_posts( array(
+                    'posts_per_page' => get_option('posts_per_page'),
+                    'suppress_filters' => true
+                ));
+
+                foreach ($posts as $post) : 
+            ?>
+
+                    <article class="post" id="post-<?php echo esc_attr( $post->ID )?>">
+                        <h1 class="post-title"><?php echo apply_filters ('the_title',$post->post_title) ?></h1>
+                        <?php  echo apply_filters ('the_content',$post->post_content)?>
+                    </article>
+                    
+                <?php endforeach ?>
+
+                
+                <?php echo do_shortcode("[button color='yellow' link='/archive']Older article[/button]") ?>
+
+
+
+
+                <?php 
                 // echo '<pre>';
                 // print_r( $posts );
                 // echo'</pre>';
-            ?>
+                 ?>            
         </div>
 
     <?php endwhile ?>
